9491bc72d797d9115070a1989dc9b014c1dddd0b
Gio Mungcal <gcmungcal@gmail.com>
2024-08-27 22:28:00 +0800

    completed backbone

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import "./App.css";
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+import Category from "./sidebar/Category/Category";
 
 function App() {
   return (
     <>
-      <h1>Hi</h1>
+      <h1 className="text-6xl">His</h1>
+      <Navigation />
+      <Products />
+      <Recommended />
+      <Category />
     </>
   );
 }
---
+++ src/Navigation/Nav.jsx
@@ -0,0 +1,7 @@
+import "./Nav.css";
+
+function Navigation() {
+  return <div>Navigation</div>;
+}
+
+export default Navigation;
